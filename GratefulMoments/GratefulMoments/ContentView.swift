b83cd610c83d83cb81f8305b54cb92124bf025c0
Oriol Angelet <uriaeri@gmail.com>
2025-12-10 10:33:58 +0100

    Initial commit, includes color & image assets

---
+++ GratefulMoments/GratefulMoments/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GratefulMoments
+//
+//  Created by Uri on 10/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GratefulMoments/GratefulMoments/GratefulMomentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GratefulMomentsApp.swift
+//  GratefulMoments
+//
+//  Created by Uri on 10/12/25.
+//
+
+import SwiftUI
+
+@main
+struct GratefulMomentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
