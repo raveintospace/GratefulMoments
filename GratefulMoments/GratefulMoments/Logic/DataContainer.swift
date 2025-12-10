4f504c117528a3bba6f0c5193ad172bc8c97f00a
Oriol Angelet <uriaeri@gmail.com>
2025-12-10 11:07:05 +0100

    Feat: Created class DataContainer & model Moment

---
+++ GratefulMoments/GratefulMoments/Logic/DataContainer.swift
@@ -0,0 +1,55 @@
+//
+//  DataContainer.swift
+//  GratefulMoments
+//
+//  Created by Uri on 10/12/25.
+//
+
+import SwiftData
+import SwiftUI
+
+@Observable
+@MainActor
+class DataContainer {
+    let modelContainer: ModelContainer
+
+    var context: ModelContext {
+        modelContainer.mainContext
+    }
+
+    init(includeSampleMoments: Bool = false) {
+        let schema = Schema([
+            Moment.self,
+        ])
+
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: includeSampleMoments)
+
+        do {
+            modelContainer = try ModelContainer(for: schema, configurations: [modelConfiguration])
+
+            if includeSampleMoments {
+                loadSampleMoments()
+            }
+            try context.save()
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }
+
+    private func loadSampleMoments() {
+        for moment in Moment.sampleData {
+            context.insert(moment)
+        }
+    }
+}
+
+private let sampleContainer = DataContainer(includeSampleMoments: true)
+
+extension View {
+    /// Adds sample data container to previews
+    func sampleDataContainer() -> some View {
+        self
+            .environment(sampleContainer)
+            .modelContainer(sampleContainer.modelContainer)
+    }
+}
