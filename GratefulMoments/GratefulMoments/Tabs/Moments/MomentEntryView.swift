d9b62351d85b8ccd0df45e2da346569e00f4a071
Oriol Angelet <uriaeri@gmail.com>
2025-12-10 10:50:44 +0100

    Feat: Created MomentEntryView

---
+++ GratefulMoments/GratefulMoments/Tabs/Moments/MomentEntryView.swift
@@ -0,0 +1,81 @@
+//
+//  MomentEntryView.swift
+//  GratefulMoments
+//
+//  Created by Uri on 10/12/25.
+//
+
+import SwiftUI
+import PhotosUI
+
+struct MomentEntryView: View {
+    
+    @State private var title = ""
+    @State private var note = ""
+    @State private var newImage: PhotosPickerItem?
+    @State private var imageData: Data?
+
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                contentStack
+            }
+            .scrollDismissesKeyboard(.interactively)
+            .navigationTitle("Grateful For")
+        }
+    }
+}
+
+#if DEBUG
+#Preview {
+    MomentEntryView()
+}
+#endif
+
+extension MomentEntryView {
+
+    private var contentStack: some View {
+        VStack(alignment: .leading) {
+            TextField(text: $title) {
+                Text("Title (Required)")
+            }
+            .font(.title)
+            .bold()
+            .padding(.top, 40)
+
+            Divider()
+
+            TextField("Log your small wins", text: $note, axis: .vertical)
+                .multilineTextAlignment(.leading)
+                .lineLimit(5...Int.max)
+
+            photoPicker
+        }
+        .padding()
+    }
+
+    private var photoPicker: some View {
+        PhotosPicker(selection: $newImage) {
+            Group {
+                if let imageData, let uiImage = UIImage(data: imageData) {
+                    Image(uiImage: uiImage)
+                        .resizable()
+                        .scaledToFit()
+                } else {
+                    Image(systemName: "photo.badge.plus.fill")
+                        .font(.largeTitle)
+                        .frame(height: 250)
+                        .frame(maxWidth: .infinity)
+                        .background(Color(white: 0.4, opacity: 0.32))
+                }
+            }
+            .clipShape(RoundedRectangle(cornerRadius: 16))
+        }
+        .onChange(of: newImage) {
+            guard let newImage else { return }
+            Task {
+                imageData = try await newImage.loadTransferable(type: Data.self)
+            }
+        }
+    }
+}
