c34174e24202fbf314ea50efcf5fa02790573cd4
Oriol Angelet <uriaeri@gmail.com>
2025-12-11 10:49:59 +0100

    Feat: Created BadgeDetailView

---
+++ GratefulMoments/GratefulMoments/Tabs/Achievements/BadgeDetailView.swift
@@ -0,0 +1,41 @@
+//
+//  BadgeDetailView.swift
+//  GratefulMoments
+//
+//  Created by Uri on 11/12/25.
+//
+
+import SwiftUI
+
+struct BadgeDetailView: View {
+
+    var badge: Badge
+
+    var body: some View {
+        VStack(spacing: 8) {
+            Image(badge.details.image)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 140, height: 140)
+
+            Text(badge.details.title)
+                .font(.title)
+                .bold()
+
+            Text(badge.details.congratulatoryMessage)
+                .font(.body)
+
+            Spacer()
+        }
+        .padding()
+        .frame(width: 320, height: 410)
+        .multilineTextAlignment(.center)
+        .foregroundStyle(.white)
+        .background(badge.details.color.opacity(0.8))
+        .clipShape(RoundedRectangle(cornerRadius: 16))
+    }
+}
+
+#Preview {
+    BadgeDetailView(badge: .sample)
+}
