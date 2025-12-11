bf72a6d747c002aad77bd835d4b421ded0a8d119
Oriol Angelet <uriaeri@gmail.com>
2025-12-11 12:05:31 +0100

    Feat: Updated ContentView to TabView type

--- GratefulMoments/GratefulMoments/ContentView.swift
+++ GratefulMoments/GratefulMoments/ContentView.swift
@@ -9,16 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            Tab("Moments", image: "MomentsTab") {
+                MomentsView()
+            }
+            Tab("Achievements", systemImage: "medal.fill") {
+                AchievementsView()
+            }
         }
-        .padding()
     }
 }
 
+#if DEBUG
 #Preview {
     ContentView()
+        .sampleDataContainer()
 }
+#endif
