abcd303ccdb6dc513da2131e158afa67e61bffdf
Oriol Angelet <uriaeri@gmail.com>
2025-12-11 11:34:17 +0100

    Feat: added badges when loading sample moments

--- GratefulMoments/GratefulMoments/Logic/DataContainer.swift
+++ GratefulMoments/GratefulMoments/Logic/DataContainer.swift
@@ -33,7 +33,7 @@ class DataContainer {
             try badgeManager.loadBadgesIfNeeded()
             
             if includeSampleMoments {
-                loadSampleMoments()
+                try loadSampleMoments()
             }
 
             try context.save()
@@ -43,9 +43,10 @@ class DataContainer {
         }
     }
 
-    private func loadSampleMoments() {
+    private func loadSampleMoments() throws {
         for moment in Moment.sampleData {
             context.insert(moment)
+            try badgeManager.unlockBadges(newMoment: moment)
         }
     }
 }
