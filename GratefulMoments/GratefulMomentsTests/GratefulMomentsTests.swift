6d9985e1503b82774321cb17fe533a8ca3efcb34
Oriol Angelet <uriaeri@gmail.com>
2025-12-12 12:03:55 +0100

    Fix: Updated logic in StreakCalculator to solve bugs

--- GratefulMoments/GratefulMomentsTests/GratefulMomentsTests.swift
+++ GratefulMoments/GratefulMomentsTests/GratefulMomentsTests.swift
@@ -14,17 +14,37 @@ struct StreakCalculatorTests {
     let streakCalculator = StreakCalculator()
     let now = Date.now
 
-    @Test func testCalculations() {
-        let days = [-2, -1]
-        let expectedStreak = 2
+    struct Input {
+        let expectedStreak: Int
+        let days: [Int]
+    }
 
-        let moments = days.map {
+    @Test("Streak calculations",
+          arguments: [
+            Input(expectedStreak: 2, days: [-2, -1]),
+            Input(expectedStreak: 1, days: [0]),
+            Input(expectedStreak: 1, days: [-1]),
+            Input(expectedStreak: 0, days: [-2]),
+
+
+            Input(expectedStreak: 1, days: [0, 0]),
+            Input(expectedStreak: 1, days: [-1, -1]),
+            Input(expectedStreak: 0, days: [-2, -2]),
+
+
+            Input(expectedStreak: 3, days: [-2, -1, 0]),
+            Input(expectedStreak: 2, days: [-3, -1, 0]),
+            Input(expectedStreak: 3, days: [-3, -2, -1]),
+            Input(expectedStreak: 2, days: [-4, -2, -1])
+          ]) func testCalculations(input: Input) {
+
+        let moments = input.days.map {
             let date = Calendar.current.date(byAdding: .day, value: $0, to: now)!
             return Moment(title: "", note: "", timestamp: date)
         }
 
         let streak = streakCalculator.calculateStreak(for: moments)
-        #expect(streak == expectedStreak)
+        #expect(streak == input.expectedStreak, "\(input.days)")
 
     }
 
